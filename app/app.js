90c306bd28e27325cf5d7cfe9b8df43b348f6945
Rodrigo Ehlers <rodrigoehlers@outlook.com>
2018-04-17 14:35:18 +0200

    added videojs logs

--- app/app.js
+++ app/app.js
@@ -25,17 +25,69 @@ const DEFAULT_OPTIONS = {
   aspectRatio: '16:9'
 };
 
+let VAST_URL;
+
 function onPlayerReady() {
-  videojs.log('Player ready.');
-  this.on('ended', () => videojs.log('Video ended.'));
+  this.on('firstplay', () => videojs.log('\'firstplay\''));
+  this.on('play', () => videojs.log('\'play\''));
+  this.on('pause', () => videojs.log('\'pause\''));
+  this.on('ended', () => videojs.log('\'ended\''));
+  videojs.log('\'ready\'');
 
   this.vastClient({
-    adTagUrl: "/vast/vast.xml",
+    adTagUrl: VAST_URL ? VAST_URL : "/vast/vast.xml",
     playAdAlways: true
   });
 }
 
+function params() {
+  const url = new URL(document.URL);
+  VAST_URL = url.searchParams.get('vast');
+}
+
+function setup() {
+  $('#nvast').val(VAST_URL ? VAST_URL : '');
+  listeners();
+}
+
+function listeners() {
+  $($('input')[0]).on('keyup', event => {
+    const { target, keyCode } = event;
+    const { value } = target;
+    if (keyCode === 13) {
+      history.pushState(null, null, (value.length ? '/?vast=' + encodeURIComponent(value) : '/'));
+      window.location = (value.length ? '/?vast=' + encodeURIComponent(value): '/');
+    }
+    event.preventDefault();
+  });
+}
+
+function logger(args) {
+  const timestamp = new Date();
+  const time = `${timestamp.getHours()}:${timestamp.getMinutes()}:${timestamp.getSeconds()}.${timestamp.getMilliseconds()}`;
+  let message = '';
+  if (args [0] === 'VIDEOJS:') {
+    message += time + ' - <span class="videojs-log">' + args[0] + ' ' + args[1].toUpperCase() + '</span>';
+  }
+  if (message.length) $('#clog').append('<li>' + message + '</li>');
+}
+
 $(() => {
+  params();
+  setup();
+
+  if (window.console && console) {
+    for (let c in console) {
+      if (typeof console[c] === 'function') {
+        const cx = console[c]
+          console[c] = function () {
+            logger(arguments);
+            cx.apply(this, arguments)
+          }
+      }
+    }
+  }
+
   const customOptions = {};
   const player = videojs('video-id', Object.assign(DEFAULT_OPTIONS, customOptions), onPlayerReady);
 });
--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const socket = require('socket.io');
 const app = express();
 
+// Heroku port
+const PORT = process.env.PORT || 5000;
+
 app.use('/public', express.static(__dirname + '/app'));
 app.use('/vast', express.static(__dirname + '/vast'));
 app.get('/vast', (request, response) => response.setHeader('Access-Control-Allow-Origin', request.get('origin')));
 app.get('/', (request, response) => response.sendFile(__dirname + '/app/app.html'));
 
-const server = app.listen(process.env.PORT || 5000, () => console.log('Ready!'));
+const server = app.listen(PORT, () => console.log('Ready at port', PORT, '!'));
 
 const io = socket.listen(server);
